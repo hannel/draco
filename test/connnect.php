777cd5bde2a4c06be67d8e2e2bc2c7d13e6575a1
hannel <hannel.mk@gmail.com>
2013-10-11 18:05:11 -0400

    'hello'

---
+++ test/connnect.php
@@ -0,0 +1,31 @@
+<?php
+
+//Organization master parameters
+	const ORGANIZATION_NAME = 'Name';
+	const ORGANIZATION_NAME_HELPTEXT = 'Please enter the name of the organization';
+	const ORGANIZATION_ABOUT = 'organization description';
+	const ORGANIZATION_ABOUT_HELPTEXT = 'Please enter the description of the organization';
+	const ORGANIZATION_URL = 'url';
+	const ORGANIZATION_URL_HELPTEXT = 'Please enter the url of the organization';
+	const ORGANIZATION_SPECIALITIES = 'specialities';
+	const ORGANIZATION_SPECIALITIES_HELPTEXT = 'Please enter the specialities of the organization';
+	
+	
+	
+	const ORGANIZATION_TYPE = 'type';
+	const ORGANIZATION_TYPE_OPTIONS = 'non profit, business';
+	const ORGANIZATION_TYPE_HELPTEXT = 'Please select the type of your organizqation';
+		
+	const ORGANIZATION_INDUSTRY = 'industry';
+	const ORGANIZATION_INDUSTRY_OPTIONS = 'IT,Banking,Manufacturing,Services,Entertainment,Marketing and advertisement,Insurance, Hospitality';
+	const ORGANIZATION_INDUSTRY_HELPTEXT = 'Please select the industry type of your organizqation';
+	
+	const ORGANIZATION_SIZE = 'size';
+	const ORGANIZATION_SIZE_OPTIONS = 'SME, MNC, small, medium, large';
+	const ORGANIZATION_SIZE_HELPTEXT = 'Please select the size of your organizqation';
+	
+	const ORGANIZATION_SUBSCRIPTION_ID = 'subscription id';
+	const ORGANIZATION_SUBSCRIPTION_ID_OPTIONS = 'gold, premium ';
+	const ORGANIZATION_SUBSCRIPTION_ID_HELPTEXT = 'Please select the subscription type of your organizqation';
+  
+?>
